a6576751b22f4133b54cc3fbfb30c46b3fe3a361
Umar Drive <umardrive9988@gmail.com>
2025-08-06 22:03:11 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'exeyxlrneqxfsxjprrdu.supabase.co',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
