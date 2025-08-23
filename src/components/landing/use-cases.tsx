03f7ae343440e891891a698a8ea041d8ffd6023f
Umar Drive <umardrive9988@gmail.com>
2025-08-23 20:29:04 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="contai

--- src/components/landing/use-cases.tsx
+++ src/components/landing/use-cases.tsx
@@ -135,9 +135,10 @@ const Feature = ({
   return (
     <div
       className={cn(
-        "flex flex-col lg:border-r py-10 relative group/feature dark:border-neutral-800",
-        (index === 0 || index === 4) && "lg:border-l dark:border-neutral-800",
-        index < 4 && "lg:border-b dark:border-neutral-800"
+        "flex flex-col md:border-r py-10 relative group/feature dark:border-neutral-800",
+        (index === 0 || index === 4) && "md:border-l dark:border-neutral-800",
+        index < 4 && "md:border-b dark:border-neutral-800",
+        "border-b"
       )}
     >
       {index < 4 && (
