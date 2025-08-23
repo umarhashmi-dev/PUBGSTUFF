56b5154a0a61c454facdb81daf10840f6880ad59
Umar Drive <umardrive9988@gmail.com>
2025-08-23 19:43:25 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <BentoGrid classNa

--- src/components/landing/bento-grid-section.tsx
+++ src/components/landing/bento-grid-section.tsx
@@ -30,119 +30,52 @@ export default function BentoGridSection() {
 }
 
 const SkeletonOne = () => {
-  const variants = {
-    initial: {
-      x: 0,
-    },
-    animate: {
-      x: 10,
-      rotate: 5,
-      transition: {
-        duration: 0.2,
-      },
-    },
-  };
-  const variantsSecond = {
-    initial: {
-      x: 0,
-    },
-    animate: {
-      x: -10,
-      rotate: -5,
-      transition: {
-        duration: 0.2,
-      },
-    },
-  };
-
   return (
-    <motion.div
-      initial="initial"
-      whileHover="animate"
+    <div
       className="flex flex-1 w-full h-full min-h-[6rem] flex-col space-y-2"
     >
-      <motion.div
-        variants={variants}
+      <div
         className="flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2  items-center space-x-2 bg-white dark:bg-black"
       >
         <div className="h-6 w-6 rounded-full bg-gradient-to-r from-pink-500 to-violet-500 shrink-0" />
         <div className="w-full bg-gray-100 h-4 rounded-full dark:bg-neutral-900" />
-      </motion.div>
-      <motion.div
-        variants={variantsSecond}
+      </div>
+      <div
         className="flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2 items-center space-x-2 w-3/4 ml-auto bg-white dark:bg-black"
       >
         <div className="w-full bg-gray-100 h-4 rounded-full dark:bg-neutral-900" />
         <div className="h-6 w-6 rounded-full bg-gradient-to-r from-pink-500 to-violet-500 shrink-0" />
-      </motion.div>
-      <motion.div
-        variants={variants}
+      </div>
+      <div
         className="flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2 items-center space-x-2 bg-white dark:bg-black"
       >
         <div className="h-6 w-6 rounded-full bg-gradient-to-r from-pink-500 to-violet-500 shrink-0" />
         <div className="w-full bg-gray-100 h-4 rounded-full dark:bg-neutral-900" />
-      </motion.div>
-    </motion.div>
+      </div>
+    </div>
   );
 };
 const SkeletonTwo = () => {
-  const variants = {
-    initial: {
-      width: 0,
-    },
-    animate: {
-      width: "100%",
-      transition: {
-        duration: 0.2,
-      },
-    },
-    hover: {
-      width: ["0%", "100%"],
-      transition: {
-        duration: 2,
-      },
-    },
-  };
   const arr = new Array(6).fill(0);
   return (
-    <motion.div
-      initial="initial"
-      animate="animate"
-      whileHover="hover"
+    <div
       className="flex flex-1 w-full h-full min-h-[6rem] flex-col space-y-2"
     >
       {arr.map((_, i) => (
-        <motion.div
+        <div
           key={"skelenton-two" + i}
-          variants={variants}
           style={{
             maxWidth: Math.random() * (100 - 40) + 40 + "%",
           }}
           className="flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2  items-center space-x-2 bg-neutral-100 dark:bg-black w-full h-4"
-        ></motion.div>
+        ></div>
       ))}
-    </motion.div>
+    </div>
   );
 };
 const SkeletonThree = () => {
-  const variants = {
-    initial: {
-      backgroundPosition: "0 50%",
-    },
-    animate: {
-      backgroundPosition: ["0, 50%", "100% 50%", "0 50%"],
-    },
-  };
   return (
-    <motion.div
-      initial="initial"
-      animate="animate"
-      variants={variants}
-      transition={{
-        duration: 5,
-        repeat: Infinity,
-        repeatType: "reverse",
-      }}
+    <div
       className="flex flex-1 w-full h-full min-h-[6rem] rounded-lg flex-col space-y-2"
       style={{
         background:
@@ -150,40 +83,16 @@ const SkeletonThree = () => {
         backgroundSize: "400% 400%",
       }}
     >
-      <motion.div className="h-full w-full rounded-lg"></motion.div>
-    </motion.div>
+      <div className="h-full w-full rounded-lg"></div>
+    </div>
   );
 };
 const SkeletonFour = () => {
-  const first = {
-    initial: {
-      x: 20,
-      rotate: -5,
-    },
-    hover: {
-      x: 0,
-      rotate: 0,
-    },
-  };
-  const second = {
-    initial: {
-      x: -20,
-      rotate: 5,
-    },
-    hover: {
-      x: 0,
-      rotate: 0,
-    },
-  };
   return (
-    <motion.div
-      initial="initial"
-      animate="animate"
-      whileHover="hover"
+    <div
       className="flex flex-1 w-full h-full min-h-[6rem] flex-row space-x-2"
     >
-      <motion.div
-        variants={first}
+      <div
         className="h-full w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center"
       >
         <img
@@ -199,8 +108,8 @@ const SkeletonFour = () => {
         <p className="border border-red-500 bg-red-100 dark:bg-red-900/20 text-red-600 text-xs rounded-full px-2 py-0.5 mt-4">
           Delusional
         </p>
-      </motion.div>
-      <motion.div className="h-full relative z-20 w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center">
+      </div>
+      <div className="h-full relative z-20 w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center">
         <img
           src="https://pbs.twimg.com/profile_images/1417752099488636931/cs2R59eW_400x400.jpg"
           alt="avatar"
@@ -214,9 +123,8 @@ const SkeletonFour = () => {
         <p className="border border-green-500 bg-green-100 dark:bg-green-900/20 text-green-600 text-xs rounded-full px-2 py-0.5 mt-4">
           Sensible
         </p>
-      </motion.div>
-      <motion.div
-        variants={second}
+      </div>
+      <div
         className="h-full w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center"
       >
         <img
@@ -232,44 +140,16 @@ const SkeletonFour = () => {
         <p className="border border-orange-500 bg-orange-100 dark:bg-orange-900/20 text-orange-600 text-xs rounded-full px-2 py-0.5 mt-4">
           Helpless
         </p>
-      </motion.div>
-    </motion.div>
+      </div>
+    </div>
   );
 };
 const SkeletonFive = () => {
-  const variants = {
-    initial: {
-      x: 0,
-    },
-    animate: {
-      x: 10,
-      rotate: 5,
-      transition: {
-        duration: 0.2,
-      },
-    },
-  };
-  const variantsSecond = {
-    initial: {
-      x: 0,
-    },
-    animate: {
-      x: -10,
-      rotate: -5,
-      transition: {
-        duration: 0.2,
-      },
-    },
-  };
-
   return (
-    <motion.div
-      initial="initial"
-      whileHover="animate"
+    <div
       className="flex flex-1 w-full h-full min-h-[6rem] flex-col space-y-2"
     >
-      <motion.div
-        variants={variants}
+      <div
         className="flex flex-row rounded-2xl border border-neutral-100 dark:border-white/[0.2] p-2  items-start space-x-2 bg-white dark:bg-black"
       >
         <img
@@ -283,15 +163,14 @@ const SkeletonFive = () => {
           There are a lot of cool framerworks out there like React, Angular,
           Vue, Svelte that can make your life ....
         </p>
-      </motion.div>
-      <motion.div
-        variants={variantsSecond}
+      </div>
+      <div
         className="flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2 items-center justify-end space-x-2 w-3/4 ml-auto bg-white dark:bg-black"
       >
         <p className="text-xs text-neutral-500">Use PHP.</p>
         <div className="h-6 w-6 rounded-full bg-gradient-to-r from-pink-500 to-violet-500 shrink-0" />
-      </motion.div>
-    </motion.div>
+      </div>
+    </div>
   );
 };
 const items = [
