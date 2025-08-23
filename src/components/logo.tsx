662340d892b60918ee9465c1e4ea489055687d09
Umar Drive <umardrive9988@gmail.com>
2025-08-23 21:03:31 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Logo>
    
    with the t

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,13 +1,12 @@
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <Link href="/" className={`flex items-center ${className}`}>
+    <Link href="/" className={cn("flex items-center hover-shimmer-button p-2 rounded-lg", className)}>
       <span className="text-xl font-bold font-headline text-foreground">
         PUBGSTUFF
       </span>
     </Link>
   );
 }
-
-    
