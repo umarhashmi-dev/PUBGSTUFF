5d70bfe48aac440a58487b5451a098d4037597fa
Umar Drive <umardrive9988@gmail.com>
2025-08-23 19:38:25 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <BentoGrid classNa

--- src/components/ui/bento-grid.tsx
+++ src/components/ui/bento-grid.tsx
@@ -37,7 +37,7 @@ export const BentoGridItem = ({
     <div
       className={cn(
         "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border justify-between flex flex-col space-y-4",
-        "relative",
+        "relative overflow-hidden",
         className
       )}
     >
@@ -57,4 +57,3 @@ export const BentoGridItem = ({
     </div>
   );
 };
-
