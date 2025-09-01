55c1bcec9205a061335925647e6fad09991b6599
Umar Drive <umardrive9988@gmail.com>
2025-09-01 11:41:52 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,6 @@
 
 'use client';
+import React from "react";
 import Footer from "@/components/landing/footer";
 import Header from "@/components/landing/header";
 import { Button } from "@/components/ui/button";
