575fac9611e3e00b4e9ccec467f81f3d16e889cc
Umar Drive <umardrive9988@gmail.com>
2025-09-02 09:07:47 +0000

    The design and everything is perfect but as you can see when you impleme

--- src/components/product-layout.tsx
+++ src/components/product-layout.tsx
@@ -7,10 +7,10 @@ import { CurrencySwitcher } from "./currency-switcher";
 export function ProductLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="relative">
-            <aside className="fixed top-1/2 left-4 -translate-y-1/2 z-50 hidden md:block">
+            <aside className="fixed top-1/2 left-4 -translate-y-1/2 z-50">
                <CurrencySwitcher />
             </aside>
-            <div className="md:pl-28">
+            <div>
                 {children}
             </div>
         </div>
