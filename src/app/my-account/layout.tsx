708f3a303651d33bb0944eac7cafbd7d1b7703f9
Umar Drive <umardrive9988@gmail.com>
2025-09-06 21:31:23 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/app/my-account/layout.tsx
+++ src/app/my-account/layout.tsx
@@ -1,5 +1,6 @@
 
 'use client';
+import React from 'react';
 import Footer from "@/components/landing/footer";
 import Header from "@/components/landing/header";
 import RequireAuth from "@/components/require-auth";
