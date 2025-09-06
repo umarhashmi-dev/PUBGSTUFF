df0b058906e50f2b9b4f1fad56373c601c75c934
Umar Drive <umardrive9988@gmail.com>
2025-09-06 11:30:37 +0000

    same use the original  logos and colors for these two button in the sign

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,7 +12,6 @@ import React from "react";
 import { Logo } from "@/components/logo";
 import Header from "@/components/landing/header";
 import { cn } from "@/lib/utils";
-import { IconBrandGoogle, IconBrandFacebook } from "@tabler/icons-react";
 import { Card } from "@/components/ui/card";
 
 export default function LoginPage() {
@@ -119,7 +118,13 @@ export default function LoginPage() {
                           type="button"
                           onClick={() => handleSocialSignIn("google")}
                       >
-                          <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+                           <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" className="h-4 w-4">
+                            <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
+                            <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.42-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
+                            <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
+                            <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
+                            <path fill="none" d="M0 0h48v48H0z"></path>
+                          </svg>
                           <span className="text-sm text-neutral-700 dark:text-neutral-300">
                           Google
                           </span>
@@ -130,7 +135,9 @@ export default function LoginPage() {
                           type="button"
                           onClick={() => handleSocialSignIn("facebook")}
                       >
-                          <IconBrandFacebook className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+                           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-4 w-4">
+                              <path fill="#1877F2" d="M22.675 0h-21.35c-0.732 0-1.325 0.593-1.325 1.325v21.351c0 0.731 0.593 1.325 1.325 1.325h11.494v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463 0.099 2.795 0.143v3.24l-1.918 0.001c-1.504 0-1.795 0.715-1.795 1.763v2.313h3.587l-0.467 3.622h-3.12v9.293h6.116c0.73 0 1.323-0.593 1.323-1.325v-21.35c0-0.732-0.593-1.325-1.323-1.325z"/>
+                          </svg>
                           <span className="text-sm text-neutral-700 dark:text-neutral-300">
                           Facebook
                           </span>
